c3c1f8900bbd444ce2e8a3ec2105e86c690dc465
mite085 <78145241+mite085@users.noreply.github.com>
2025-06-07 23:54:39 +0800

    fix: Event修改
    输入相关事件移植到input模块

--- src/input/input_events.h
+++ src/input/input_events.h
@@ -5,6 +5,74 @@
 
 namespace mite {
 
+class WindowFocusEvent : public Event {
+ public:
+  WindowFocusEvent() = default;
+  EVENT_CLASS_TYPE(WindowFocus)
+  EVENT_CLASS_CATEGORY(EventCategoryWindow)
+};
+
+class WindowLostFocusEvent : public Event {
+ public:
+  WindowLostFocusEvent() = default;
+  EVENT_CLASS_TYPE(WindowLostFocus)
+  EVENT_CLASS_CATEGORY(EventCategoryWindow)
+};
+
+class WindowMovedEvent : public Event {
+ public:
+  WindowMovedEvent() = default;
+  EVENT_CLASS_TYPE(WindowMoved)
+  EVENT_CLASS_CATEGORY(EventCategoryWindow)
+};
+
+class MouseMoveEvent : public Event {
+ public:
+  MouseMoveEvent(double xpos, double ypos) : xpos(xpos), ypos(ypos) {}
+  double GetXPos() const;
+  double GetYPos() const;
+
+  EVENT_CLASS_TYPE(MouseMoved)
+  EVENT_CLASS_CATEGORY(EventCategoryMouse)
+ private:
+  double xpos, ypos;
+};
+
+class MouseButtonEvent : public Event {
+ public:
+  MouseButtonEvent(int button, int action, int mods, double xpos, double ypos)
+      : button(button), action(action), mods(mods), xpos(xpos), ypos(ypos)
+  {
+  }
+  int GetButton() const;
+  int GetAction() const;
+  int GetMods() const;
+  double GetXPos() const;
+  double GetYPos() const;
+
+  EVENT_CLASS_TYPE(MouseButtonReleased)
+  EVENT_CLASS_CATEGORY(EventCategoryMouse)
+ private:
+  int button, action, mods;
+  double xpos, ypos;
+};
+
+class KeyEvent : public Event {
+ public:
+  KeyEvent(int key, int scancode, int action, int mods)
+      : key(key), scancode(scancode), action(action), mods(mods)
+  {
+  }
+  int GetKey() const;
+  int GetScancode() const;
+  int GetAction() const;
+  int GetMods() const;
+
+  EVENT_CLASS_TYPE(KeyReleased)
+  EVENT_CLASS_CATEGORY(EventCategoryMouse)
+ private:
+  int key, scancode, action /*GLFW_PRESS, GLFW_RELEASE, GLFW_REPEAT*/, mods /*ÐÞÊÎ¼ü×´Ì¬*/;
+};
 };
 
 #endif
