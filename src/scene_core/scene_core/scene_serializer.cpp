db609dda1c42bd16e2db9a9985738f06c4ea7bfb
mite085 <78145241+mite085@users.noreply.github.com>
2025-06-13 23:06:50 +0800

    feat: Cereal include

---
+++ src/scene_core/scene_core/scene_serializer.cpp
@@ -0,0 +1,5 @@
+#include "scene_serializer.h"
+
+namespace mite {
+
+};
---
+++ src/scene_core/scene_core/scene_serializer.h
@@ -0,0 +1,14 @@
+#ifndef MITE_SCENE_SERIALIZER
+#define MITE_SCENE_SERIALIZER
+
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/cereal.hpp>
+#include <entt/entt.hpp>
+#include "headers/headers.h"
+
+namespace mite {
+
+};
+
+#endif
