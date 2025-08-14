904cd1015281f2c6579182ec57db7d98f8a5110c
mite085 <78145241+mite085@users.noreply.github.com>
2025-08-14 23:03:37 +0800

    feat: 新增Gizmo配套的InputProcessor

---
+++ src/engine/input/input_processors/gizmo_input_processor.cpp
@@ -0,0 +1,61 @@
+#include "gizmo_input_processor.h"
+#include "GLFW/glfw3.h"
+
+namespace mite {
+
+GizmoInputProcessor::GizmoInputProcessor(std::shared_ptr<Camera> camera,
+                                         glm::mat4 &transformMatrix)
+    : m_Camera(std::move(camera)), m_TransformMatrix(transformMatrix)
+{
+  // 订阅事件
+  m_EventSubscriptions.Subscribe<MouseButtonPressedEvent>(BIND_DISPATCH_FN(handleMouseButton));
+  m_EventSubscriptions.Subscribe<KeyPressedEvent>(BIND_DISPATCH_FN(handleKey));
+}
+
+void GizmoInputProcessor::Update(float deltaTime)
+{
+  m_Gizmo.Manipulate(m_TransformMatrix, *m_Camera, m_ViewportPos, m_ViewportSize);
+}
+
+void GizmoInputProcessor::SetViewportRect(const glm::vec2 &pos, const glm::vec2 &size)
+{
+  m_ViewportPos = pos;
+  m_ViewportSize = size;
+}
+
+bool GizmoInputProcessor::handleMouseButton(MouseButtonPressedEvent &e)
+{
+  // 只处理左键点击且Gizmo未被使用时
+  if (e.GetButton() == GLFW_MOUSE_BUTTON_LEFT && !m_Gizmo.IsOver()) {
+    // TODO: 实现选择逻辑
+    return false;
+  }
+  return false;
+}
+
+bool GizmoInputProcessor::handleKey(KeyPressedEvent &e)
+{
+  const bool pressed = (e.GetEventType() == EventType::KEY_PRESSED);
+  if (!pressed)
+    return false;
+
+  // Gizmo操作切换
+  switch (e.GetKey()) {
+    case GLFW_KEY_W:  // 平移
+      m_Gizmo.SetOperation(ImGuizmo::OPERATION::TRANSLATE);
+      return true;
+    case GLFW_KEY_E:  // 旋转
+      m_Gizmo.SetOperation(ImGuizmo::OPERATION::ROTATE);
+      return true;
+    case GLFW_KEY_R:  // 缩放
+      m_Gizmo.SetOperation(ImGuizmo::OPERATION::SCALE);
+      return true;
+    case GLFW_KEY_T:  // 切换本地/世界空间
+      m_Gizmo.SetMode(m_Gizmo.GetMode() == ImGuizmo::MODE::LOCAL ? ImGuizmo::MODE::WORLD :
+                                                                   ImGuizmo::MODE::LOCAL);
+      return true;
+  }
+  return false;
+}
+
+}  // namespace mite
---
+++ src/engine/input/input_processors/gizmo_input_processor.h
@@ -0,0 +1,56 @@
+// gizmo_input_processor.h
+#ifndef MITE_GIZMO_INPUT_PROCESSOR
+#define MITE_GIZMO_INPUT_PROCESSOR
+
+#include "ImGuizmo.h"
+#include "basic_data/camera.h"
+#include "basic_data/gizmo.h"
+#include "input/input_processor.h"
+#include "input/input_event.h"
+
+namespace mite {
+
+/**
+ * @brief Gizmo输入处理器
+ *
+ * 修改点：
+ * - 不再直接操作Entity
+ * - 使用glm::mat4作为变换矩阵
+ */
+class GizmoInputProcessor : public InputProcessor {
+ public:
+  GizmoInputProcessor(std::shared_ptr<Camera> camera, glm::mat4 &transformMatrix);
+
+  // InputProcessor接口
+  int GetPriority() const override
+  {
+    return InputPriority::GIZMO;
+  }
+  const std::string &GetID() const override
+  {
+    static const std::string id = "GizmoProcessor";
+    return id;
+  }
+
+  // 每帧更新
+  void Update(float deltaTime);
+
+  // 配置方法
+  void SetViewportRect(const glm::vec2 &pos, const glm::vec2 &size);
+
+ protected:
+  // 事件处理
+  bool handleMouseButton(MouseButtonPressedEvent &e);
+  bool handleKey(KeyPressedEvent &e);
+
+ private:
+  std::shared_ptr<Camera> m_Camera;
+  glm::mat4 &m_TransformMatrix;  // 引用外部变换矩阵
+  Gizmo m_Gizmo;
+  glm::vec2 m_ViewportPos;
+  glm::vec2 m_ViewportSize;
+};
+
+}  // namespace mite
+
+#endif
