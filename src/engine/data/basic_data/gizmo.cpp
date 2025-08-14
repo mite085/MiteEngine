ecd03b33385ca46f05db39574619a8fde90ee4ee
mite085 <78145241+mite085@users.noreply.github.com>
2025-08-14 23:03:05 +0800

    feat: 新增独立的Gizmo控件

---
+++ src/engine/data/basic_data/gizmo.cpp
@@ -0,0 +1,77 @@
+#include "gizmo.h"
+
+namespace mite {
+
+Gizmo::Gizmo()
+{
+  ImGuizmo::Enable(true);
+}
+
+void Gizmo::SetOperation(ImGuizmo::OPERATION operation)
+{
+  m_Operation = operation;
+}
+
+void Gizmo::SetMode(ImGuizmo::MODE mode)
+{
+  m_Mode = mode;
+}
+
+bool Gizmo::Manipulate(glm::mat4 &transformMatrix,
+                       const Camera &camera,
+                       const glm::vec2 &viewportPos,
+                       const glm::vec2 &viewportSize)
+{
+  // 设置Gizmo操作空间
+  ImGuizmo::SetOrthographic(camera.GetProjectionType() == Camera::ProjectionType::Orthographic);
+  ImGuizmo::SetRect(viewportPos.x, viewportPos.y, viewportSize.x, viewportSize.y);
+
+  // 执行Gizmo操作
+  bool manipulated = ImGuizmo::Manipulate(glm::value_ptr(camera.GetViewMatrix()),
+                                          glm::value_ptr(camera.GetProjectionMatrix()),
+                                          m_Operation,
+                                          m_Mode,
+                                          glm::value_ptr(transformMatrix));
+
+  m_IsUsing = ImGuizmo::IsUsing();
+  m_IsOver = ImGuizmo::IsOver();
+
+  return manipulated;
+}
+
+void Gizmo::DecomposeTransform(const glm::mat4 &transform,
+                               glm::vec3 &translation,
+                               glm::vec3 &rotation,
+                               glm::vec3 &scale)
+{
+  // 使用glm的分解函数
+  glm::quat orientation;
+  glm::vec3 skew;
+  glm::vec4 perspective;
+  glm::decompose(transform, scale, orientation, translation, skew, perspective);
+
+  // 将四元数转换为欧拉角
+  rotation = glm::degrees(glm::eulerAngles(orientation));
+}
+
+bool Gizmo::IsUsing() const
+{
+  return m_IsUsing;
+}
+
+bool Gizmo::IsOver() const
+{
+  return m_IsOver;
+}
+
+ImGuizmo::OPERATION Gizmo::GetOperation() const
+{
+  return m_Operation;
+}
+
+ImGuizmo::MODE Gizmo::GetMode() const
+{
+  return m_Mode;
+}
+
+}  // namespace mite
---
+++ src/engine/data/basic_data/gizmo.h
@@ -0,0 +1,54 @@
+#ifndef MITE_GIZMO_H
+#define MITE_GIZMO_H
+
+#include "ImGuizmo.h"
+#include "basic_data/camera.h"
+
+namespace mite {
+
+/**
+ * @brief Gizmo工具类，封装ImGuizmo的核心功能
+ */
+class Gizmo {
+ public:
+  Gizmo();
+
+  // 设置操作模式
+  void SetOperation(ImGuizmo::OPERATION operation);
+  void SetMode(ImGuizmo::MODE mode);
+
+  /**
+   * @brief 执行Gizmo操作
+   * @param transformMatrix 输入输出的变换矩阵
+   * @param camera 使用的相机
+   * @param viewportPos 视口位置(左上角)
+   * @param viewportSize 视口尺寸
+   * @return 是否进行了操作
+   */
+  bool Manipulate(glm::mat4 &transformMatrix,
+                  const Camera &camera,
+                  const glm::vec2 &viewportPos,
+                  const glm::vec2 &viewportSize);
+
+  // 状态获取
+  bool IsUsing() const;
+  bool IsOver() const;
+  ImGuizmo::OPERATION GetOperation() const;
+  ImGuizmo::MODE GetMode() const;
+
+  // 矩阵分解工具
+  static void DecomposeTransform(const glm::mat4 &transform,
+                                 glm::vec3 &translation,
+                                 glm::vec3 &rotation,
+                                 glm::vec3 &scale);
+
+ private:
+  ImGuizmo::OPERATION m_Operation = ImGuizmo::OPERATION::TRANSLATE;
+  ImGuizmo::MODE m_Mode = ImGuizmo::MODE::LOCAL;
+  bool m_IsUsing = false;
+  bool m_IsOver = false;
+};
+
+}  // namespace mite
+
+#endif  // MITE_GIZMO_H
