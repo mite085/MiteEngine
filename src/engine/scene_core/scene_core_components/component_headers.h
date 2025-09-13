922d344853df259e774bb5a9f220a44e83d325f1
mite085 <78145241+mite085@users.noreply.github.com>
2025-09-13 22:23:41 +0800

    feat: 新增包围盒组件规划

--- src/engine/scene_core/scene_core_components/component_headers.h
+++ src/engine/scene_core/scene_core_components/component_headers.h
@@ -3,12 +3,13 @@
 
 #include "camera_component.h"
 #include "destroy_component.h"
-#include "hierarchy_component.h"
+#include "boundingbox_component.h"
 #include "id_component.h"
 #include "material_component.h"
 #include "mesh_component.h"
 #include "tag_component.h"
 #include "transform_component.h"
+#include "visibility_component.h"
 
 namespace mite {
 
