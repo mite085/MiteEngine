10c18f86f9ebd6a8003fd331ab9cf35f5020fb3f
mite085 <78145241+mite085@users.noreply.github.com>
2025-09-16 20:07:28 +0800

    feat: 新增编辑器的对象创建工厂规划

---
+++ src/editor/object_factor.h
@@ -0,0 +1,23 @@
+
+// 仅编辑器需要“创建对象”
+// 对Runtime而言，Entity和Component级别的抽象就已经足够了
+class EditorObjectFactory {
+public:
+    static Entity CreateCamera(SceneRegistry& registry) {
+        Entity entity = registry.CreateEntity();
+        registry.AddComponent<TransformComponent>(entity);
+        registry.AddComponent<CameraComponent>(entity);
+        registry.AddComponent<IDComponent>(entity);
+        registry.AddComponent<TagComponent>(entity, "Camera");
+        return entity;
+    }
+    
+    static Entity CreateModel(SceneRegistry& registry, MeshHandle mesh) {
+        Entity entity = registry.CreateEntity();
+        registry.AddComponent<TransformComponent>(entity);
+        registry.AddComponent<MeshComponent>(entity, mesh);
+        registry.AddComponent<IDComponent>(entity);
+        registry.AddComponent<TagComponent>(entity, "Model");
+        return entity;
+    }
+};
