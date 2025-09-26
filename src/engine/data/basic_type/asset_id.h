cd7c2149320866ad3a2d30921251329b66c4f734
mite085 <78145241+mite085@users.noreply.github.com>
2025-09-26 22:50:33 +0800

    feat: 新增类型安全的AssetID

---
+++ src/engine/data/basic_type/asset_id.h
@@ -0,0 +1,62 @@
+#ifndef MITE_ASSET_ID
+#define MITE_ASSET_ID
+
+#include "uuid/mite_uuid.h"
+
+namespace mite {
+
+// 类型安全的资产ID包装器
+template<typename Tag> class TypedAssetID {
+ private:
+  UUID id_;
+
+ public:
+  TypedAssetID() : id_(UUIDGenerator::Generate()) {}
+  explicit TypedAssetID(const UUID &id) : id_(id) {}
+  explicit TypedAssetID(const char *str) : id_(UUIDGenerator::Generate(str)) {}
+
+  const UUID &GetUUID() const
+  {
+    return id_;
+  }
+  bool IsValid() const
+  {
+    return id_.IsValid();
+  }
+
+  // 比较操作
+  bool operator==(const TypedAssetID &other) const
+  {
+    return id_ == other.id_;
+  }
+  bool operator!=(const TypedAssetID &other) const
+  {
+    return id_ != other.id_;
+  }
+  bool operator<(const TypedAssetID &other) const
+  {
+    return id_ < other.id_;
+  }
+
+  // 哈希支持
+  struct Hash {
+    size_t operator()(const TypedAssetID &id) const
+    {
+      return std::hash<UUID>()(id.id_);
+    }
+  };
+};
+
+// 具体的资产类型标签
+struct TextureAssetTag {};
+struct MaterialAssetTag {};
+struct ModelAssetTag {};
+
+// 类型安全的资产ID
+using TextureAssetID = TypedAssetID<TextureAssetTag>;
+using MaterialAssetID = TypedAssetID<MaterialAssetTag>;
+using ModelAssetID = TypedAssetID<ModelAssetTag>;
+
+}  // namespace mite
+
+#endif
