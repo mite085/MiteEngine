50d488f157891330c9f1842aa2bfb1e235375cd6
mite085 <78145241+mite085@users.noreply.github.com>
2025-10-19 22:40:10 +0800

    feat: 新增view相关事件

--- src/engine/data/basic_event/render_event.h
+++ src/engine/data/basic_event/render_event.h
@@ -1,16 +1,18 @@
 #ifndef MITE_RENDER_EVENT
 #define MITE_RENDER_EVENT
 
+#include "basic_data/transform.h"
 #include "basic_shader/framebuffer.h"
 #include "subscription_group.h"
 
 namespace mite {
 /**
- * 视口尺寸改变事件
+ * @brief Viewport尺寸改变事件
+ * @note SceneView订阅事件修改相机宽高比，Pipeline订阅事件修改帧缓冲尺寸
  */
-class ViewPortResizeEvent : public Event {
+class ViewportResizeEvent : public Event {
  public:
-  explicit ViewPortResizeEvent(const glm::vec2 &size) : m_Size(size) {}
+  explicit ViewportResizeEvent(const glm::vec2 &size) : m_Size(size) {}
   const glm::vec2 &GetSize() const
   {
     return m_Size;
@@ -18,13 +20,64 @@ class ViewPortResizeEvent : public Event {
   EVENT_CLASS_CATEGORY(EVENT_CATEGORY_RENDER)
   Event *Clone() const override
   {
-    return new ViewPortResizeEvent(m_Size);
+    return new ViewportResizeEvent(m_Size);
   }
 
  private:
   glm::vec2 m_Size;
 };
+/**
+ * @brief Viewport拾取物体事件（仅记录UV）
+ * @note SceneView订阅事件执行RayCast
+ */
+class ViewportPickedEvent : public Event {
+ public:
+  explicit ViewportPickedEvent(const glm::vec2 &uv) : m_UV(uv) {}
+  const glm::vec2 &GetUV() const { return m_UV; }
+  EVENT_CLASS_CATEGORY(EVENT_CATEGORY_RENDER)
+  Event *Clone() const override { return new ViewportPickedEvent(m_UV); }
 
+ private:
+  glm::vec2 m_UV;
+};
+/**
+ * @brief Viewport相机操控事件
+ * @note SceneView订阅事件执行相机变换
+ */
+class ViewportCameraUpdateEvent : public Event {
+ public:
+  explicit ViewportCameraUpdateEvent(const Transform cameraTransform, const float cameraZoom)
+      : m_CameraTransform(cameraTransform), m_CameraZoom(cameraZoom)
+  {
+  }
+  const Transform &GetCameraTransform() const { return m_CameraTransform; }
+  float GetCameraZoom() const { return m_CameraZoom; }
+  EVENT_CLASS_CATEGORY(EVENT_CATEGORY_RENDER)
+  Event *Clone() const override
+  {
+    return new ViewportCameraUpdateEvent(m_CameraTransform, m_CameraZoom);
+  }
+
+ private:
+  Transform m_CameraTransform;
+  float m_CameraZoom;
+};
+
+/**
+ * @brief ViewportPicked物体操控事件
+ * @note SceneView订阅事件执行物体变换
+ */
+class ViewportPickedUpdateEvent : public Event {
+ public:
+  explicit ViewportPickedUpdateEvent(const Transform transform) : m_Transform(transform) {
+  }
+  const Transform &GetTransform() const { return m_Transform; }
+  EVENT_CLASS_CATEGORY(EVENT_CATEGORY_RENDER)
+  Event *Clone() const override { return new ViewportPickedUpdateEvent(m_Transform); }
+
+ private:
+  Transform m_Transform;
+};
 
 /**
  * 渲染完成事件，每帧结束触发，可通过订阅该事件获取FBO
